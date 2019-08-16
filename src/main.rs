2b3cc13384555984060b876c93e2718a1af6ce3d
equal-l2 <eng.equall2@gmail.com>
2019-08-17 01:15:16 +0900

    Read from stdin when file name is not given

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::io::Read;
+use std::fmt::Write;
 
 const BYTES_PER_LINE: usize = 16;
 
 fn output_line(elems: &[u8]) {
     let rem = BYTES_PER_LINE - elems.len();
 
-    print!("\x1b[31m|\x1b[0m");
+    print!("\x1b[31m│\x1b[0m");
     for b in elems {
         print!("\x1b[48;5;{}m ", b);
     }
@@ -13,15 +14,17 @@ fn output_line(elems: &[u8]) {
     if rem != 0 {
         print!("{}", " ".repeat(rem));
     }
-    print!("\x1b[31m|\x1b[0m");
+    print!("\x1b[31m│\x1b[0m");
 
+    let mut s1 = String::with_capacity(3*BYTES_PER_LINE);
     for b in elems {
-        print!(" {:02X}", b);
+        write!(s1, "{:02X} ", b).unwrap();
     }
     if rem != 0 {
-        print!("{}", " ".repeat(rem*3));
+        write!(s1, "{}", " ".repeat(rem*3)).unwrap();
     }
-    print!(" \x1b[31m|\x1b[0m");
+    s1.pop();
+    print!("{}\x1b[31m│\x1b[0m", s1);
 
     for b in elems {
         let c = if *b >= 32 && *b <= 126 { *b as char } else { ' ' };
@@ -30,16 +33,13 @@ fn output_line(elems: &[u8]) {
     if rem != 0 {
         print!("{}", " ".repeat(rem));
     }
-    print!("\x1b[31m|\x1b[0m");
+    print!("\x1b[31m│\x1b[0m");
     println!();
 }
 
-fn main() {
-    let fname = std::env::args().nth(1).unwrap();
-    let bytes = std::fs::File::open(fname).unwrap().bytes();
-
+fn run(reader: impl Read) {
     let mut v = Vec::with_capacity(BYTES_PER_LINE);
-    for b in bytes {
+    for b in reader.bytes() {
         v.push(b.unwrap());
         if v.len() == BYTES_PER_LINE {
             output_line(&v);
@@ -49,4 +49,14 @@ fn main() {
     if !v.is_empty() {
         output_line(&v);
     }
+}
+
+fn main() {
+    let fname = std::env::args().nth(1);
+    if let Some(i) = fname {
+        run(std::fs::File::open(i).unwrap());
+    } else {
+        run(std::io::stdin());
+    };
+
 }
