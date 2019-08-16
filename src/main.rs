8fe4ddf26e64de57380d42b889bd9ff19fb1ed47
equal-l2 <eng.equall2@gmail.com>
2019-08-17 01:32:53 +0900

    A character for non-printable characters can now be set

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
+use std::io;
 use std::io::Read;
 use std::fmt::Write;
 
 const BYTES_PER_LINE: usize = 16;
 
-fn output_line(elems: &[u8]) {
+fn output_line(elems: &[u8], empty_char: char) {
     let rem = BYTES_PER_LINE - elems.len();
 
+    // graphical expression
+    let width = 1;
     print!("\x1b[31m│\x1b[0m");
     for b in elems {
-        print!("\x1b[48;5;{}m ", b);
+        print!("\x1b[48;5;{}m{}", b, " ".repeat(width));
     }
     print!("\x1b[0m");
     if rem != 0 {
-        print!("{}", " ".repeat(rem));
+        print!("{}", " ".repeat(rem*width));
     }
     print!("\x1b[31m│\x1b[0m");
 
+    // bytes (hex)
     let mut s1 = String::with_capacity(3*BYTES_PER_LINE);
     for b in elems {
         write!(s1, "{:02X} ", b).unwrap();
@@ -26,8 +30,9 @@ fn output_line(elems: &[u8]) {
     s1.pop();
     print!("{}\x1b[31m│\x1b[0m", s1);
 
+    // chars
     for b in elems {
-        let c = if *b >= 32 && *b <= 126 { *b as char } else { ' ' };
+        let c = if *b >= 32 && *b <= 126 { *b as char } else { empty_char };
         print!("{}", c);
     }
     if rem != 0 {
@@ -37,26 +42,33 @@ fn output_line(elems: &[u8]) {
     println!();
 }
 
-fn run(reader: impl Read) {
+fn run(reader: impl Read, empty_char: char) {
     let mut v = Vec::with_capacity(BYTES_PER_LINE);
     for b in reader.bytes() {
         v.push(b.unwrap());
         if v.len() == BYTES_PER_LINE {
-            output_line(&v);
+            output_line(&v, empty_char);
             v.clear();
         }
     }
     if !v.is_empty() {
-        output_line(&v);
+        output_line(&v, empty_char);
     }
 }
 
 fn main() {
     let fname = std::env::args().nth(1);
-    if let Some(i) = fname {
-        run(std::fs::File::open(i).unwrap());
+    let empty_char = std::env::args().nth(2);
+    let empty_char = if let Some(i) = empty_char {
+        i.chars().nth(0).unwrap()
     } else {
-        run(std::io::stdin());
+        '.'
+    };
+
+    if let Some(i) = fname {
+        run(io::BufReader::new(std::fs::File::open(i).unwrap()), empty_char);
+    } else {
+        run(io::BufReader::new(io::stdin()), empty_char);
     };
 
 }
